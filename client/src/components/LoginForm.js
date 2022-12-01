b572f6a6ed2a191f8e0a514e5dcf7e08c7539a5c
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-12-01 09:29:03 -0800

    adds mutation, queries in client, and schemas in server

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,14 +1,16 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { useMutation } from "@apollo/react-hooks";
+import { loginUser } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [login, { err }] = useMutation(loginUser)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
--- server/models/Book.js
+++ server/models/Book.js
@@ -19,12 +19,12 @@ const bookSchema = new Schema({
   image: {
     type: String,
   },
-  link: {
-    type: String,
-  },
   title: {
     type: String,
     required: true,
+  },
+  link: {
+    type: String,
   },
 });
 
